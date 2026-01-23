05db7b22021498a3784db4276aa977b7c915b9df
pratchjp <alone9364@gmail.com>
2026-01-23 14:49:08 +0700

    add item entitybase

---
+++ StockApi/Entities/Item.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockApi.Entities
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("ItemCode")]
+        public string ItemCode { get; set; } = string.Empty;
+
+        [Required]
+        [Column("Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Column("Category")]
+        public string Category {  get; set; } = string.Empty;
+
+        [Required]
+        [Column("Unit")]
+        public string Unit { get; set; } = string.Empty;
+    }
+}
